22474321f155fc2994fd67e818514980f2ac1266
Angela Liu <angelamuliu@gmail.com>
2015-09-07 14:31:56 -0400

    Base model code for request and request item

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,2 +1,14 @@
 class Request < ActiveRecord::Base
+
+    # Relationships
+    belongs_to :user
+    belongs_to :shop
+    has_many :items, :through => :request_items
+
+    # Validations
+    validates_presence_of :shop_id
+
+    # Scopes
+    scope :chronological, order(:created_at)
+
 end
